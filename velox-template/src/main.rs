89558c1ffbcefc1fbb85f428fdee8cf3a687bd17
dev-sinpy <tdevyanshu9@gmail.com>
2021-02-02 17:42:56 +0530

    update config path

--- velox-template/src/main.rs
+++ velox-template/src/main.rs
@@ -1,6 +1,6 @@
 use velox::AppBuilder;
 
 fn main() {
-    let mut app = AppBuilder::from_config(include_str!("../velox-config.json").to_string()).build();
+    let mut app = AppBuilder::from_config(include_str!("../velox.conf.json").to_string()).build();
     app.run().unwrap();
 }
